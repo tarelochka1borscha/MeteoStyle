f0485e5ead5e63d7d2f7aec77b90ca551a759914
Pelmen <alexi@PELMEN>
2024-08-04 01:50:29 +0300

    1.0.1 Created main page

---
+++ MeteoStyle/Pages/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MeteoStyle.Pages;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(string currentCity, int selectedType)
+	{
+		InitializeComponent();
+	}
+}
--- MeteoStyle/Pages/WelcomePage.xaml.cs
+++ MeteoStyle/Pages/WelcomePage.xaml.cs
@@ -1,12 +1,22 @@
-﻿namespace MeteoStyle
+﻿using MeteoStyle.Pages;
+
+namespace MeteoStyle
 {
-    public partial class MainPage : ContentPage
+    public partial class WelcomePage : ContentPage
     {
         int count = 0;
 
-        public MainPage()
+        public WelcomePage()
         {
             InitializeComponent();
+        }
+
+        private async void GoToHomePage(object sender, EventArgs e)
+        {
+            int selectedType = 3;
+            if (RBCold.IsChecked) selectedType = 1;
+            if (RBWarm.IsChecked) selectedType = 2;
+            await Navigation.PushAsync(new MainPage(EntryCity.Text, selectedType));
         }
     }
 
